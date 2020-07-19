171c2f55077b2d9ccf02af2bbf42e757ecc8aa22
SamuelParrales <68031754+SamuelParrales@users.noreply.github.com>
2020-07-19 17:53:07 -0500

    Subido
    Archivos Subidos

---
+++ TiendaWeb/src/main/java/sepp/TiendaWeb/TiendaWebApplication.java
@@ -0,0 +1,13 @@
+package sepp.TiendaWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaWebApplication.class, args);
+	}
+
+}
---
+++ TiendaWeb/src/test/java/sepp/TiendaWeb/TiendaWebApplicationTests.java
@@ -0,0 +1,13 @@
+package sepp.TiendaWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
