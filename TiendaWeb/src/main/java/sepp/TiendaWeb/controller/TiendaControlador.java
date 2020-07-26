e84bb80679e4ade6b7f22d1e248ab591b71f8942
Luis Dominguez <Luis Dominguez@DESKTOP-UH6B8ER.mshome.net>
2020-07-26 17:16:44 -0500

    Inciando el controlador e indexando el html principal

---
+++ TiendaWeb/src/main/java/sepp/TiendaWeb/controller/TiendaControlador.java
@@ -0,0 +1,26 @@
+package sepp.TiendaWeb.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+
+import sepp.TiendaWeb.repos.ProductRepo;
+import sepp.TiendaWeb.service.PictureService;
+
+
+
+@Controller
+@RequestMapping("/TiendaWeb")
+public class TiendaControlador {
+	/*@Autowired
+	private ProductRepo repo;*/
+	
+	@Autowired
+	PictureService picService;
+	
+	@RequestMapping("")
+	public String index() {
+		return "index";
+	}
+}
